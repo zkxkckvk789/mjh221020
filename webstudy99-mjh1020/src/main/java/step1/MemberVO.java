0fc623782cf708282f6784c741438a86eda4bd19
HAYOUNG LEE <hyl9410@gmail.com>
2022-10-18 17:39:14 +0900

    MemberVO 이름 변수 추가, getter/setter추가

--- webstudy99-mjh1020/src/main/java/step1/MemberVO.java
+++ webstudy99-mjh1020/src/main/java/step1/MemberVO.java
@@ -1,5 +1,15 @@
 package step1;
 
 public class MemberVO {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
